33b56a18862aa9a853535865bc2a4885a615908e
Mazen-Serli <mazen.natour@serli.com>
2025-01-15 17:14:04 +0100

    Data modules
    data modules (blackboards) for generic types

--- src/mdd.c
+++ src/mdd.c
@@ -22,6 +22,21 @@ MDD_int MDD_int_init(const int val) {
 	return m;
 }
 
+
+/**
+ * Initializes a shared generic data
+ *\param val[in] initial value of the shared data
+ *\return the new shared generic data, memory allocation is done inside the function
+ */
+MDD_generic MDD_generic_init(int size) {
+	MDD_generic m=(MDD_generic)malloc(sizeof(struct s_MDD_generic));
+	m->val=malloc(size);
+	m->dirty=0;
+	m->size=size;
+	pthread_mutex_init(&(m->mutex),0);
+	return m;
+}
+
 /**
  * Writes in a shared int
  *\param mdd[in,out] the shared data
@@ -33,6 +48,20 @@ void MDD_int_write(MDD_int mdd, const int val) {
 	mdd->dirty=1;
 	pthread_mutex_unlock(&(mdd->mutex));
 }
+
+
+/**
+ * Writes in a shared generic data
+ *\param mdd[in,out] the shared data
+ *\param val[in] value to write in the shared data
+ */
+void MDD_generic_write(MDD_generic mdd, void *val) {
+	pthread_mutex_lock(&(mdd->mutex));
+	memcpy(mdd->val,val,mdd->size);
+	mdd->dirty=1;
+	pthread_mutex_unlock(&(mdd->mutex));
+}
+
 /**
  * Reads in a shared int
  *\param mdd[in] the shared data
@@ -46,6 +75,22 @@ int MDD_int_read(MDD_int mdd) {
 	pthread_mutex_unlock(&(mdd->mutex));
 	return val;
 }
+
+/**
+ * Reads in a shared generic data
+ *\param mdd[in] the shared data
+ *\return value of the shared data
+ */
+void *MDD_generic_read(MDD_generic mdd) {
+	void *val=malloc(mdd->size);
+	pthread_mutex_lock(&(mdd->mutex));
+	memcpy(val,mdd->val,mdd->size);
+	mdd->dirty=0;
+	pthread_mutex_unlock(&(mdd->mutex));
+	return val;
+}
+
+
 /**
  * Reads in a shared int, while returning if it was dirty
  *\param mdd[in] the shared data
@@ -60,5 +105,21 @@ int MDD_int_read2(MDD_int mdd,int *val) {
 	mdd->dirty=0;
 	pthread_mutex_unlock(&(mdd->mutex));
 	return dirty;
+}
 
+
+/**
+ * Reads in a shared generic data, while returning if it was dirty
+ *\param mdd[in] the shared data
+ *\param val[out] value of the shared data
+ *\return  1 if the shared data was modified since last reading (i.e., dirty), 0 else
+ */
+int MDD_generic_read2(MDD_generic mdd,void *val) {
+  	int dirty;
+	pthread_mutex_lock(&(mdd->mutex));
+	memcpy(val,mdd->val,mdd->size);
+	dirty=mdd->dirty;
+	mdd->dirty=0;
+	pthread_mutex_unlock(&(mdd->mutex));
+	return dirty;
 }
--- src/mdd.h
+++ src/mdd.h
@@ -14,12 +14,27 @@ typedef struct s_MDD_int {
 	pthread_mutex_t mutex;
 } * MDD_int;
 
+typedef struct s_MDD_generic {
+	void *val;
+	int dirty;
+	int size;
+	pthread_mutex_t mutex;
+} * MDD_generic;
+
 /**
  * Initializes a shared int
  *\param val[in] initial value of the shared data
  *\return the new shared int, memory allocation is done inside the function
  */
 MDD_int MDD_int_init(const int val);
+
+
+/**
+ * Initializes a shared generic data
+ *\param val[in] initial value of the shared data
+ *\return the new shared generic data, memory allocation is done inside the function
+ */
+MDD_generic MDD_generic_init(int size);
 
 /**
  * Writes in a shared int
@@ -30,12 +45,28 @@ MDD_int MDD_int_init(const int val);
 void MDD_int_write(MDD_int mdd, const int val);
 
 /**
+ * Writes in a shared generic data
+ *\param mdd[in,out] the shared data
+ *\param val[in] value to write in the shared data
+ * Side effect: shared generic data is considered dirty until next reading
+ */
+void MDD_generic_write(MDD_generic mdd, void *val);
+
+/**
  * Reads in a shared int
  *\param mdd[in] the shared data
  *\return value of the shared data
  * Side effect: shared int is now considered clean
  */
 int MDD_int_read(MDD_int mdd);
+
+/**
+ * Reads in a shared generic data
+ *\param mdd[in] the shared data
+ *\return value of the shared data
+ * Side effect: shared generic data is now considered clean
+ */
+void *MDD_generic_read(MDD_generic mdd);
 
 /**
  * Reads in a shared int, while returning if it was dirty
@@ -45,4 +76,15 @@ int MDD_int_read(MDD_int mdd);
  * Side effect: shared int is now considered clean
  */
 int MDD_int_read2(MDD_int mdd,int *val);
+
+/**
+ * Reads in a shared generic data, while returning if it was dirty
+ *\param mdd[in] the shared data
+ *\param val[out] value of the shared data
+ *\return  1 if the shared data was modified since last reading (i.e., dirty), 0 else
+ * Side effect: shared generic data is now considered clean
+ */
+int MDD_generic_read2(MDD_generic mdd,void *val);
+
+
 #endif /* MDD_H_ */
